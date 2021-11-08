8396b8a81c2837dafe9b586671f9b9122fc84483
Andrew Busel <buselax@gmail.com>
2021-11-08 13:21:40 -0500

    added json server and allows added favorites that persist

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function Card({ company }) {
+function Card({ company, logo, name }) {
   return (
     <>
       <div className="ui card">
         <div className="ui slide masked reveal image">
           <img
             alt="logo"
-            src="https://static.finnhub.io/logo/87cb30d8-80df-11ea-8951-00000000092a.png"
+            src= {logo}
             className="visible content"
           />
-          <p className="hidden content">hi</p>
+          <p className="hidden content">{name}</p>
         </div>
         <div className="content">
           <a className="header">{company}</a>
