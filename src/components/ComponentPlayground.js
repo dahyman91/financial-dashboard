3ff930a3fbfd6344830e34714ff6ceee31a01db5
Daniel Hyman <dahyman91@gmail.com>
2021-11-10 14:03:15 -0500

    styled cards

--- src/components/ComponentPlayground.js
+++ src/components/ComponentPlayground.js
@@ -4,17 +4,19 @@ import Widget from "./Widget";
 
 function ComponentPlayground({ selectedTicker }) {
   return (
-    <div
-      style={{
-        borderTop: "1px solid black",
-        width: "100%",
-        position: "absolute",
-        top: "200vh",
-      }}
-    >
-      <BasicFinancials selectedTicker={selectedTicker} />
-      <Widget selectedTicker={selectedTicker} />
-    </div>
+    <>
+      <div
+        style={{
+          borderTop: "1px solid black",
+          width: "100%",
+          position: "absolute",
+          top: "200vh",
+        }}
+      >
+        <BasicFinancials selectedTicker={selectedTicker} />
+        <Widget selectedTicker={selectedTicker} />
+      </div>
+    </>
   );
 }
 
